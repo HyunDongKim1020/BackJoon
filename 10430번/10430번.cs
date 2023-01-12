fb3aa48803594263e62af166aea3757445cf669f
KHD1020 <bnm00533@gmail.com>
2023-01-12 11:19:31 +0900

    2023-01-12/백준 문제 풀이 커밋

---
+++ 10430번/10430번.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] s = Console.ReadLine().Split();
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+            int c = int.Parse(s[2]);
+
+            Console.WriteLine((a + b) % c);
+            Console.WriteLine((a % c) + (b % c) % c);
+            Console.WriteLine((a * b) % c);
+            Console.WriteLine((a % c) * (b % c) % c);
+        }
+    }
+}
---
+++ 2588번/2588번.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputA = Console.ReadLine();
+            string inputB = Console.ReadLine();
+
+            int a = int.Parse(inputA);
+            int b = int.Parse(inputB);
+
+            Console.WriteLine((a * b));
+
+        }
+    }
+}
