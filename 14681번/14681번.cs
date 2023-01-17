10432290810d7aee3803c32d53e38828278caf06
KHD1020 <bnm00533@gmail.com>
2023-01-17 17:39:02 +0900

    2023-01-17/ 백준 문제풀이 커밋

---
+++ 14681번/14681번.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if(x > 0 && y > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if(x > 0 && y < 0)
+            {
+                Console.WriteLine("4");
+            }
+            else if(x < 0 && y > 0)
+            {
+                Console.WriteLine("2");
+            }
+            else
+            {
+                Console.WriteLine("3");
+            }
+        }
+    }
+}
