bb7e2b634bca3fa0d7fa25f80e6a58e56af5001d
KHD1020 <bnm00533@gmail.com>
2023-01-25 17:56:56 +0900

    2023-01-25/백준 문제 풀이 커밋

---
+++ 2884번/2884번.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] time = Console.ReadLine().Split();
+
+            int h = int.Parse(time[0]);
+            int m = int.Parse(time[1]);
+
+            if (h <= 0 || h <= 23 || m <= 0 || m <= 59)
+            {
+                if (m < 45)
+                {
+                    if (h <= 0)
+                    {
+                        h += 24;
+                        m += 15;
+                        h--;
+                    }
+                    else
+                    {
+                        m += 15;
+                        h--;
+                    }
+                }
+              
+                else
+                {
+                    m -= 45;
+                }
+            }
+
+
+            Console.WriteLine($"{h} {m}");
+        }
+    }
+}
